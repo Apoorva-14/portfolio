8e7b6820d2c46d4c1feac413362c9b6686f6c42a
Apoorva Sharma <apoorvasharma141195@gmail.com>
2023-03-17 14:42:05 +0530

    feat: backdrop for mobile menu

--- handlers.js
+++ handlers.js
@@ -3,6 +3,7 @@ export function setupMobileMenuToggle(button, menu) {
   const toggleMobileMenuState = () => {
     isOpen = !isOpen;
     menu.classList.toggle("hidden");
+    document.getElementById("menu-backdrop").classList.toggle("hidden");
   };
   button.addEventListener("click", () => toggleMobileMenuState());
 }
